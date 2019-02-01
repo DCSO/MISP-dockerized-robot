c66db02fcd3811f11ce9d624dc28f7705b321bc6
8ear <github@8ear.de>
2019-02-01 15:02:41 +0100

    Add End message of before_install script

--- .ci/01_before_install.sh
+++ .ci/01_before_install.sh
@@ -49,3 +49,4 @@ git config --global user.name "MISP-dockerized-bot"
     echo "$START Docker login..."
     [ ! -z "$CUSTOM_REGISTRY_URL" -a ! -z "$CUSTOM_REGISTRY_USER" -a ! -z "$CUSTOM_REGISTRY_PW" ] && echo "$CUSTOM_REGISTRY_PW" | docker login -u "$CUSTOM_REGISTRY_USER" "$CUSTOM_REGISTRY_URL" --password-stdin;
     
+echo "$START before_install is finished."
