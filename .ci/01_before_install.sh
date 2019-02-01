f1822c8448e04eef2a88cb1d67be1cdd01114911
8ear <github@8ear.de>
2019-02-01 12:52:08 +0100

    Clean .travis folder and rename our .ci scripts

---
+++ .ci/01_before_install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+START="[before_install]"
+#########################
+#   FOR Local Testing
+#########################
+#
+#       Please do the following for the local dev tests:
+#       export VERSION=1.0.0 or any other version you want to test
+#       'docker login' with your credentials
+#
+#
+
+# Install Requirements
+echo
+echo "$START Install requirements..."
+    [ ! -z $(which apk) ] && apk add --no-cache make bash sudo git curl coreutils grep python3
+    [ ! -z $(which apt-get) ] && apt-get update; 
+    [ ! -z $(which apt-get) ] && apt-get install make bash sudo git curl coreutils grep python3
+    # Upgrade Docke
+    [ ! -z $(which apt-get) ] && apt-get install --only-upgrade docker-ce -y
+# Install docker-compose
+    # https://stackoverflow.com/questions/42295457/using-docker-compose-in-a-gitlab-ci-pipeline
+    [ -z $(which docker-compose) ] && pip3 install --no-cache-dir docker-compose
+# Show version of docker-compose:
+    docker-compose -v
+
+
+
+# Set Git Options
+echo
+echo "$START Set Git options..."
+git config --global user.name "MISP-dockerized-bot"
+
+
+# # Updating Docker configuration
+# echo
+# echo "$START Updating Docker configuration..."
+# echo '{
+#   "experimental": true,
+#   "storage-driver": "overlay2",
+#   "max-concurrent-downloads": 50,
+#   "max-concurrent-uploads": 50
+# }' | sudo tee /etc/docker/daemon.json
+#   sudo service docker restart
+
+
+# Set new Gitlab Repository
+    echo
+    echo "$START Docker login..."
+    [ ! -z "$CUSTOM_REGISTRY_URL" -a ! -z "$CUSTOM_REGISTRY_USER" -a ! -z "$CUSTOM_REGISTRY_PW" ] && echo "$CUSTOM_REGISTRY_PW" | docker login -u "$CUSTOM_REGISTRY_USER" "$CUSTOM_REGISTRY_URL" --password-stdin;
+    
--- Makefile
+++ Makefile
@@ -4,22 +4,26 @@
 .PHONY: help test-travis build tags push install notify-hub-docker-com
 
 help:
-	echo "Please use a command"
+	@echo -e "Please use a command: \n \
+		make before_install \n \
+		make build v=<2.3-debian> dev=true \n \
+		make tags \n \
+		make push \n \
+		make notify-hub.docker.com TOKEN=<TOKEN> \n \
+	"
 
-test-travis:
-	.travis/travis-cli.sh check
+
+before_install:
+	.ci/01_before_install.sh
 
 build:
-	.travis/build.sh $(v) $(dev)
+	.ci/02_build.sh $(v) $(dev)
 
 tags:
-	.travis/tagging.sh
+	.ci/03_tagging.sh
 
 push:
-	.travis/push.sh
-
-install:
-	.travis/main.sh
+	.ci/04_push.sh
 
 notify-hub-docker-com:
-	.travis/notify_hub.docker.com.sh $(TOKEN)
+	.ci/05_notify_hub.docker.com.sh $(TOKEN)
