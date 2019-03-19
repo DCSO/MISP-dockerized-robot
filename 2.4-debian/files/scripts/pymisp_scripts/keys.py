7225858e0fe0e369b1090d8e20243d127bf221b8
8ear <github@8ear.de>
2019-03-19 15:51:58 +0100

    Test 2.4

--- 2.4-debian/files/scripts/pymisp_scripts/keys.py
+++ 2.4-debian/files/scripts/pymisp_scripts/keys.py
@@ -6,5 +6,10 @@ misp_old_key = 'EHHlZzXtU8B8BV6Ugz5BNw8wyhBFgebeaDflvBzN' # The MISP auth key ca
 misp_old_verifycert = False
 
 misp_new_url = 'https://misp3.test/'
-misp_new_key = 'sAwfM2mNQBVgKOSe2yI1AQISuP2IvskQcSQZbiso' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_new_key = 'yj8AjP3dD5zCCNiXCFw106oD6ZFhWqIMpdFNZ6NV' # The MISP auth key can be found on the MISP web interface under the automation section
 misp_new_verifycert = False
+
+#MYSQL_HOST="misp-server"
+#MYSQL_DB="misp"
+#MYSQL_PASSWORD="yourpassword"
+#MYSQL_USER="misp"
