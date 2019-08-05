484572521fc6a3d108804fc0330017cd4de5e723
8ear <github@8ear.de>
2019-08-05 17:06:15 +0200

    Update test.sh

--- 2.5-debian/files/scripts/test.sh
+++ 2.5-debian/files/scripts/test.sh
@@ -6,10 +6,17 @@ set -eu
 [ -n "${2-}" ] && TEST_LOG2FILE=$(echo "$2"|cut -d = -f 2)
 
 # Variables
-NC='\033[0m' # No Color
-Light_Green='\033[1;32m'  
-STARTMSG="${Light_Green}[TEST]${NC}"
+# NC='\033[0m' # No Color
+# Light_Green='\033[1;32m'  
+STARTMSG="[TEST]"
 PROXY_IP=$(docker inspect misp-proxy|grep IPAddress| tail -1|cut -d '"' -f 4)
+MISP_DOCKERIZED_TESTBENCH_FOLDER="/srv/MISP-dockerized-testbench"
+MSG_3="Start Workers ... finished"
+MSG_2="Your MISP-dockerized server has been successfully booted."
+MSG_1="Your MISP docker has been successfully booted for the first time."
+SLEEP_TIMER=5
+DEFAULT_RETRY=100
+
 
 # Functions
 echo (){
@@ -17,61 +24,81 @@ echo (){
 }
 
 # Environment Variables
-GIT_FOLDER=${GIT_FOLDER:-"/srv/MISP-dockerized-testbench"}
-MISP_FQDN=${MISP_FQDN:-"$(grep MISP_FQDN /srv/MISP-dockerized/config/config.env |cut -d = -f 2)"}
+MISP_FQDN=${MISP_FQDN:-"$(grep MISP_FQDN /srv/MISP-dockerized/config/config.env |cut -d = -f 2|cut -d \" -f 2)"}
 MISP_BASEURL=${MISP_BASEURL:-"https://$MISP_FQDN"}
-TEST_LOGLEVEL=${TEST_LOGLEVEL:-"info"}
-TEST_LOG2FILE=${TEST_LOG2FILE:-"False"}
+TEST_LOGLEVEL=${TEST_LOGLEVEL:-"debug"}
+TEST_LOG2FILE=${TEST_LOG2FILE:-"True"}
+
+
 
 #
 #   MAIN
 #
 
-cd  "$GIT_FOLDER" || exit 1
+command echo && echo "Start Test script ... " && command echo
+# Wait until all is ready
+    [ "${CI-}" = "true" ] && echo "wait 50 seconds..." && sleep 10
+    [ "${CI-}" = "true" ] && echo "wait 40 seconds..." && sleep 10
+    [ "${CI-}" = "true" ] && echo "wait 30 seconds..." && sleep 10
+    [ "${CI-}" = "true" ] && echo "wait 20 seconds..." && sleep 10
+    [ "${CI-}" = "true" ] && echo "wait 10 seconds..." && sleep 10
 
-# generate report folder
-[ -d reports ] || mkdir reports
-
-
-# wait until misp-server is ready
-MSG_3="####################################  started Apache2"
-MSG_2="Your MISP-dockerized server has been successfully booted."
-MSG_1="Your MISP docker has been successfully booted for the first time."
-SLEEP_TIMER=5
-while true
+# Wait until misp-server is ready
+RETRY=$DEFAULT_RETRY
+until [ $RETRY -le 0 ]
 do
-    [ -z "$(docker logs misp-server 2>&1 | grep "$MSG_3")" ] || break
-    [ -z "$(docker logs misp-server 2>&1 | grep "$MSG_2")" ] || break
-    [ -z "$(docker logs misp-server 2>&1 | grep "$MSG_1")" ] || break
-    #wait x seconds
-    echo "$(date +%T) -  wait until misp-server is ready. sleep $SLEEP_TIMER seconds..."
+    # shellcheck disable=SC2143
+    [ -n "$(docker logs misp-server 2>&1 | grep "$MSG_3")" ] && break
+    # shellcheck disable=SC2143
+    [ -n "$(docker logs misp-server 2>&1 | grep "$MSG_2")" ] && break
+    # shellcheck disable=SC2143
+    [ -n "$(docker logs misp-server 2>&1 | grep "$MSG_1")" ] && break
+    command echo && echo "$(date +%T) -  Wait until misp-server is ready. sleep $SLEEP_TIMER seconds; Retry $RETRY/100..." && command echo
     docker logs --tail 10 misp-server
     sleep "$SLEEP_TIMER"
-    SLEEP_TIMER="$(expr $SLEEP_TIMER + 5)"
+    SLEEP_TIMER="$(( SLEEP_TIMER + 5))"
+    
+    # shellcheck disable=SC2004
+    RETRY=$(( $RETRY - 1))
+    if [ $RETRY -le 0 ]; then
+        >&2 echo "... ... Error: Could not connect to MISP server."
+        exit 1
+    fi
 done
 
 # Init MISP and create user
-
-while true
+RETRY=$DEFAULT_RETRY
+until [ $RETRY -le 0 ]
 do
     # copy auth_key
     AUTH_KEY="$(docker exec misp-server bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "SELECT authkey FROM users;" | head -2|tail -1')"
-    
     # initial user if all is good auth_key is return
-    [ -z "$AUTH_KEY"  ] && AUTH_KEY="$(docker exec misp-server bash -c "sudo -E /var/www/MISP/app/Console/cake userInit -q")" && echo "new Auth_Key: $AUTH_KEY"
-    
+    [ -z "$AUTH_KEY"  ] && AUTH_KEY="$(docker exec misp-server bash -c "$SUDO_WWW /var/www/MISP/app/Console/cake userInit -q")"
     # if user is initalized but mysql is not ready continue
     [ "$AUTH_KEY" = "Script aborted: MISP instance already initialised." ] && continue
-    
     # if the auth_key is save go out 
-    [ -z "$AUTH_KEY" ] || break
-
+    [ -n "$AUTH_KEY" ] && break
     # wait 5 seconds
-    echo "$(date +%T) -  wait until the test script get the authentication key." && sleep 5
+    command echo && echo "$(date +%T) -  wait until the test script get the authentication key." && command echo
+    sleep $SLEEP_TIMER
+
+    # shellcheck disable=SC2004
+    RETRY=$(( $RETRY - 1))
+    if [ $RETRY -le 0 ]; then
+        >&2 echo "... ... Error: Could not connect to MISP server."
+        exit 1
+    fi
 done
 
 
-# generate settings.json
+# Change to testbench folder
+cd  "$MISP_DOCKERIZED_TESTBENCH_FOLDER" || exit 1
+
+# Create report folder
+[ -d reports ] || mkdir reports
+[ -d logs ] || mkdir logs
+
+# Generate settings.json
 cat << EOF > settings.json
 {
     "verify_cert": "False",
@@ -86,17 +113,14 @@ cat << EOF > settings.json
 
 EOF
 # Show settings...
-cat settings.json
-
+    command echo && cat settings.json
 
 # Add MISP_FQDN to robots hosts file for ping etc.
-    ! grep -q "$PROXY_IP" /etc/hosts  && echo "Add $MISP_FQDN to $PROXY_IP in /etc/hosts" && command echo "$PROXY_IP $MISP_FQDN" >> /etc/hosts
-# Ping
-    echo "Ping $MISP_FQDN:" && ping -w 2 "$MISP_FQDN"
-    echo
-    echo "Ping misp-proxy:" && ping -w 2 misp-proxy
+    ! grep -q "$MISP_FQDN" /etc/hosts  && command echo && echo "Add $MISP_FQDN to $PROXY_IP in /etc/hosts" && command echo "$PROXY_IP $MISP_FQDN" >> /etc/hosts
+# Test if Ping works for MISP_FQDN and misp-proxy
+    command echo && echo "Ping $MISP_FQDN:" && ping -w 2 "$MISP_FQDN"
+    command echo && echo "Ping misp-proxy:" && ping -w 2 misp-proxy
 
 # Run Tests
-echo "Start Test: python3 misp-testbench.py " && python3 misp-testbench.py 
-
-exit 0
+    command echo && echo "Start Test: python3 misp-testbench.py " && python3 misp-testbench.py 
+    command echo && command echo && command echo && command echo
